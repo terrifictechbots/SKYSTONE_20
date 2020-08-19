093b0c9c1053244eae75320dc72d681b5276d9f2
terrifictechbots <terrifictechbots@gmail.com>
2020-08-19 16:57:36 -0700

    Add new code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmandaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmandaTest.java
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode;
 public class AmandaTest {
 }
 //I made this code 8/19/20 and will push it up to github -Amanda
-//I'm going to try to upload this code with these changes
+//I'm going to try to upload this code now
 
 
