352d4375b477fdf1db8a5fc70043353a06fa9bb0
terrifictechbots <42880351+terrifictechbots@users.noreply.github.com>
2020-08-21 11:24:00 -0700

    Update AmandaTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmandaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmandaTest.java
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode;
 public class AmandaTest {
 }
 //I made this code 8/19/20 and will push it up to github -Amanda
-//Testing
+//Testing 123
 //I'm going to try to upload this code now
 
