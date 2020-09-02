0408d137d401f70f5dc6df4c6ff072290f188745
terrifictechbots <terrifictechbots@gmail.com>
2020-09-02 15:02:08 -0700

    testing -amanda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmandaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmandaTest.java
@@ -4,5 +4,9 @@ public class AmandaTest {
 }
 //I made this code 8/19/20 and will push it up to github -Amanda
 //I'm going to try to upload this code now
-//Testingggggggg
+//Testing testing 123
 //I'm going to try to upload this code now
+//Amanda's test line!!!!!
+//amanda's test again
+//another amanda test
+
